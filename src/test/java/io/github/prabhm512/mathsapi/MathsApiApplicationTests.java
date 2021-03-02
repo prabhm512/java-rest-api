7171f1dd474949ef0c0ca5beeafe2c85e4d0cad8
Prabh Singh <prabhm512@gmail.com>
2021-03-02 16:28:13 +1100

    file structure setup

---
+++ src/test/java/io/github/prabhm512/mathsapi/MathsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.prabhm512.mathsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
